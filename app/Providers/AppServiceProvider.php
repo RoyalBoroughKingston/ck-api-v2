6f37063089cea7428723f40dd9ff2a2590a84b5c
Stuart Laverick <appsol@gmail.com>
2021-01-29 12:38:12 +0000

    Fix style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Use CarbonImmutable instead of Carbon.
-        Date::use (CarbonImmutable::class);
+        Date::use(CarbonImmutable::class);
 
         // Geocode.
         switch (config('ck.geocode_driver')) {

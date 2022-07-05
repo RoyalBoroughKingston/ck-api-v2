3b0e0fcdd046111d09df2d801c5a449e8bf93570
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-07-05 17:13:22 +0100

    Updated docs

--- app/Docs/Operations/Pages/IndexPageOperation.php
+++ app/Docs/Operations/Pages/IndexPageOperation.php
@@ -4,6 +4,7 @@ namespace App\Docs\Operations\Pages;
 
 use App\Docs\Parameters\FilterIdParameter;
 use App\Docs\Parameters\FilterParameter;
+use App\Docs\Parameters\IncludeParameter;
 use App\Docs\Parameters\SortParameter;
 use App\Docs\Schemas\AllSchema;
 use App\Docs\Schemas\Page\PageListItemSchema;
@@ -33,19 +34,23 @@ class IndexPageOperation extends Operation
             ->parameters(
                 FilterIdParameter::create(),
                 FilterParameter::create(null, 'parent_id')
-                    ->description('Filter by Parent ID to return descendant nodes')
-                    ->schema(Schema::string()),
+                    ->description('Filter by Parent ID to return child nodes')
+                    ->schema(Schema::string(Schema::FORMAT_UUID)),
+                FilterParameter::create(null, 'landing_page')
+                    ->description('Filter by Landing Page ID to return all descendant nodes')
+                    ->schema(Schema::string(Schema::FORMAT_UUID)),
                 FilterParameter::create(null, 'title')
                     ->description('Filter by title')
                     ->schema(Schema::string()),
                 FilterParameter::create(null, 'page_type')
                     ->description('Filter by page type')
                     ->schema(Schema::string('page_type')
-                    ->enum(
-                        Page::PAGE_TYPE_INFORMATION,
-                        Page::PAGE_TYPE_LANDING
-                    )),
-                SortParameter::create(null, ['title'], 'title')
+                            ->enum(
+                                Page::PAGE_TYPE_INFORMATION,
+                                Page::PAGE_TYPE_LANDING
+                            )),
+                SortParameter::create(null, ['title'], 'title'),
+                IncludeParameter::create(null, ['parent', 'children', 'landingPageAncestors'])
             )
             ->responses(
                 Response::ok()->content(

7ace5af16bd1b317d074e8e66009b4b14bb5a1f1
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-12-01 15:58:29 +0000

    Improved regex pattern

--- resources/views/partials/environment-warning.blade.php
+++ resources/views/partials/environment-warning.blade.php
@@ -6,7 +6,7 @@
 				<span class="govuk-warning-text__assistive">Important</span>
 				Please DO NOT make any changes to this site. This is a TEST environment used for demo purposes only. Any
 				changes made here will not be reflected on the LIVE site viewed by the public. <a
-					href="{{ preg_replace('/\b(?:((?:https?):\/\/)|www\.)([-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|])/', '${1}admin.${2}', config('app.url')) }}">Click
+					href="{{ preg_replace('/\b(?:((?:https?):\/\/)|www\.)(api\.staging\.)?([-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|])/', '${1}admin.${3}', config('app.url')) }}">Click
 					HERE</a> to access the LIVE environment.
 			</strong>
 		</div>

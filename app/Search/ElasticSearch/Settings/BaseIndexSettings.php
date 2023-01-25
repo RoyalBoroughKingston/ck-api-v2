b27f71ca8db1bd3af2c38d17ddf2472e6bc9fd4a
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2023-01-25 17:54:36 +0000

    Fix style

--- app/Search/ElasticSearch/Settings/BaseIndexSettings.php
+++ app/Search/ElasticSearch/Settings/BaseIndexSettings.php
@@ -22,7 +22,7 @@ class BaseIndexSettings
                             'lowercase',
                             'synonym',
                             'stop',
-                            'stopwords'
+                            'stopwords',
                         ],
                     ],
                 ],

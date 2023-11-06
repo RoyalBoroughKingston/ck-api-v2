b42e06b6b52a9cf413b069b9c6c3f7d3d9ab48db
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2023-11-06 12:38:42 +0000

    Altered the SQS queue name format to work with sqs suffix

--- aws/variables.py
+++ aws/variables.py
@@ -2,15 +2,15 @@ from troposphere import Join, Ref
 
 
 def create_default_queue_name_variable(environment_parameter, uuid_parameter):
-    return Join('-', [Ref(environment_parameter), Ref(uuid_parameter), 'default'])
+    return Join('-', ['default', Ref(uuid_parameter), Ref(environment_parameter)])
 
 
 def create_notifications_queue_name_variable(environment_parameter, uuid_parameter):
-    return Join('-', [Ref(environment_parameter), Ref(uuid_parameter), 'notifications'])
+    return Join('-', ['notifications', Ref(uuid_parameter), Ref(environment_parameter)])
 
 
 def create_search_queue_name_variable(environment_parameter, uuid_parameter):
-    return Join('-', [Ref(environment_parameter), Ref(uuid_parameter), 'search'])
+    return Join('-', ['search', Ref(uuid_parameter), Ref(environment_parameter)])
 
 
 def create_uploads_bucket_name_variable(environment_parameter, uuid_parameter):
